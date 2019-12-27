bbaa2a0259780253e084adc319c9ce3ab9a61584
fiddler <568200539@qq.com>
2019-12-27 11:38:25 +0800

    adjust heapify termination condition index

--- Heap/src/Main.java
+++ Heap/src/Main.java
@@ -2,16 +2,23 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        MaxHeap<Integer> maxHeap = new BinaryIndexTreeMaxHeap<>();
-        int cnt = 10000;
-        Random random = new Random();
-        for (int i = 0; i < cnt; i++) {
-            maxHeap.add(random.nextInt(0x7fff));
-        }
 
-        for (int i = 0; i < cnt; i++) {
+//        int cnt = 10000;
+//        Random random = new Random();
+//        for (int i = 0; i < cnt; i++) {
+//            maxHeap.add(random.nextInt(0x7fff));
+//        }
+//
+//        for (int i = 0; i < cnt; i++) {
+//            System.out.print(maxHeap.extractMax());
+//            System.out.print(",");
+//        }
+
+        Integer[] nums = {2, 7, 26, 25, 19, 17, 1, 90, 3, 6};
+        MaxHeap<Integer> maxHeap = new BinaryIndexTreeMaxHeap<Integer>(nums);
+        for (int i = 0; i < nums.length; i++) {
             System.out.print(maxHeap.extractMax());
-            System.out.print(",");
+            System.out.print(" ");
         }
     }
 }
